1975e687690b9d71b9aa0e6773e595ed2a72941d
Illia Kuchko and Nathan Romike <illia+nathan@>
2016-01-04 14:45:41 -0800

    final green test past

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -2,12 +2,15 @@ public class PingPong {
   public static void main(String[] args) {}
 
   public String decision(Integer number) {
-      if ( number % 3 == 0 ){
+    if ( number % 15 == 0){
+      return "pingpong";
+    }else if ( number % 3 == 0 ){
       return "ping";
     } else if (number % 5 == 0){
       return "pong";
     } else {
       return String.valueOf(number);
     }
+
   }
 }
